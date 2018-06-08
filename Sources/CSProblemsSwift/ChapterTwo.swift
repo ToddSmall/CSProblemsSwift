32ba2c20a995ba4e7c15a42671aec8d102c557d0
Todd Small <todd.andrew.small@gmail.com>
2018-06-07 22:24:44 -0700

    Remove awkward gene sequence stuff. Add binary search.

--- Sources/CSProblemsSwift/ChapterTwo.swift
+++ Sources/CSProblemsSwift/ChapterTwo.swift
@@ -1,21 +1,17 @@
-enum Nucleotide: Character, Comparable {
-    static func < (lhs: Nucleotide, rhs: Nucleotide) -> Bool {
-        return lhs.rawValue < rhs.rawValue
-    }
+func binarySearch<T: Comparable>(_ arr: [T], item: T) -> Int? {
+    var low = 0
+    var high = arr.count - 1
     
-    case A = "A"
-    case C = "C"
-    case G = "G"
-    case T = "T"
-}
-
-typealias Codon = (Nucleotide, Nucleotide, Nucleotide)
-typealias Gene = [Codon]
-
-func splitString(_ s: String, by: Int = 3) -> [String] {
-    return stride(from: 0, to: s.count, by: by).map{ i -> String in
-        let startIndex = s.index(s.startIndex, offsetBy: i)
-        let endIndex = s.index(startIndex, offsetBy: by, limitedBy: s.endIndex) ?? s.endIndex
-        return String(s[startIndex..<endIndex])
+    while low <= high {
+        let mid = (low + high) / 2
+        
+        if item < arr[mid] {
+            high = mid - 1
+        } else if item == arr[mid] {
+            return mid
+        } else {
+            low = mid + 1
+        }
     }
+    return nil
 }
--- Tests/CSProblemsSwiftTests/ChapterTwoTests.swift
+++ Tests/CSProblemsSwiftTests/ChapterTwoTests.swift
@@ -2,14 +2,15 @@ import XCTest
 @testable import CSProblemsSwift
 
 final class ChapterTwoTests: XCTestCase {
-    func testNucleotide() {
-        XCTAssertEqual(Nucleotide.C, Nucleotide.C)
-        XCTAssert(Nucleotide.A < Nucleotide.C)
-    }
-    
-    func testsplitString() {
-        let s = "Drummer"
-        XCTAssertEqual(splitString(s, by: 3), ["Dru", "mme", "r"])
-        XCTAssertEqual(splitString(s, by: 4), ["Drum", "mer"])
+    func testBinarySearch() {
+        let arr = [1, 2, 3, 4, 5, 6]
+        XCTAssertEqual(binarySearch(arr, item: 2), 1)
+        XCTAssertEqual(binarySearch(arr, item: 1), 0)
+        XCTAssertEqual(binarySearch(arr, item: 6), 5)
+        XCTAssertNil(binarySearch(arr, item: -1))
+        XCTAssertNil(binarySearch(arr, item: 7))
+
+        let emptyArr = [Int]()
+        XCTAssertNil(binarySearch(emptyArr, item: 4))
     }
 }
